272c691131897d309b72bfaf22bb0866aae49d33
Mitchell Mudd <mitchellmudd@outlook.com>
2021-05-30 12:49:29 -0400

    Started the calculator project

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-a
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(operator, x, y) {
+    if (operator === '+') {
+        return add(x, y);
+    } else if (operator === '-') {
+        return subtract(x, y);
+    } else if (operator === '*') {
+        return multiply(x, y);
+    } else if (operator === '/') {
+        return divide(x, y);
+    } else {
+        console.log('The operate function has failed.');
+    }
+}
