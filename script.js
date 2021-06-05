56b4d2c43cc8d4f34babc1ae6ab840ccd8b924ee
Mitchell Mudd <mitchellmudd@outlook.com>
2021-06-05 16:10:52 -0400

    Got the CSS working and made the clear button functional.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const inputBox = document.getElementById('output');
+const clearButton = document.getElementById('clear');
+const buttons = document.getElementsByTagName('button');
+
+function numberButtons() {
+    const zero = document.getElementById('numberZero')
+    const one = document.getElementById('one')
+    const two = document.getElementById('two')
+    const three = document.getElementById('three')
+    const four = document.getElementById('four')
+    const five = document.getElementById('five')
+    const six = document.getElementById('six')
+    const seven = document.getElementById('seven')
+    const eight = document.getElementById('eight')
+    const nine = document.getElementById('nine')
+
+}
+clearButton.addEventListener('click', clearInput)
+
+function clearInput() {
+    const inputBox = document.getElementById('output');
+    inputBox.value = ''
+}
+
 function add(x, y) {
     return x + y;
 }
